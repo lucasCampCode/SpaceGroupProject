3685c91dd1d5f7ee6e687d050b68877d4c5b8cda
lucas <umbreonbeyond@gmail.com>
2023-07-11 01:57:19 -0500

    initial config

---
+++ Source/SpaceGroupProject/SpaceGroupProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceGroupProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceGroupProject, "SpaceGroupProject" );
+ 
